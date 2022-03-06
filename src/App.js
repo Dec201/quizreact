952fa53e6b2552c88a6feeda280848c4db31b540
Dec134 <Dec201@hotmail.com>
2022-03-06 23:59:13 +0000

    quizReact-v1.1

--- src/App.js
+++ src/App.js
@@ -14,14 +14,26 @@ function App() {
 
   const [currentScore, setCurrentScore] = useState(0);
   const [questionSelector, setQuestionSelector] = useState(1);
+  const [endGameWinner, setEndGameWinner] = useState(false);
+  const [endGameLoser, setEndGameLoser] = useState(false);
 
   function failGame(){
+    setEndGameLoser(true);
     console.log("You lose");
   }
 
   function nextRound(){
+    if(currentScore < 4){
     setCurrentScore(currentScore + 1);
     setQuestionSelector(questionSelector + 1);
+    } else {
+      winEndGame();
+    }
+  }
+
+  function winEndGame(){
+    setEndGameWinner(true);
+    console.log("End Game Won!");
   }
 
 
@@ -44,7 +56,6 @@ function App() {
     <div className="MainContent">
     <h1 className="MainTitle">QuizFive</h1>
     <p>Answer all five questions correctly to win the daily QuizFive trophy!</p>
-
     {QuestionList.filter(questionFilter => questionFilter.id === questionSelector)
         .map((question, index) => {
           return (
